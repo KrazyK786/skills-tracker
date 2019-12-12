f8ff14ececd4efe0c95d26a1664fde0de2755ca4
Kristopher Johnson <krazyk786@gmail.com>
2019-12-12 16:20:28 -0600

    completed form and added post method

--- src/main/java/org/launchcode/skillstracker/controllers/SkillsTrackerController.java
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsTrackerController.java
@@ -1,54 +1,68 @@
 package org.launchcode.skillstracker.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @ResponseBody
 public class SkillsTrackerController {
 
-    @GetMapping
+    @GetMapping("/")
     public String root(){
         String htmlString = "" +
                 "<h1>Skills Tracker</h1>" +
                 "<br>" +
-                "<h2>We have a few skills that we would like to learn.  Her is the list! </h2>" +
-                "<ol>Java</ol>" +
-                "<ol>JavaScript</ol>" +
-                "<ol>Angular</ol>";
+                "<h2>We have a few skills that we would like to learn.  Here is the list! </h2>" +
+                "<ol>" +
+                "<li>Java</li>" +
+                "<li>JavaScript</li>" +
+                "<li>Angular</li>" +
+                "</ol>";
         return htmlString;
     }
 
     @GetMapping("/form")
     public String form(){
         String form = "" +
-                "<form>" +
+                "<form action='/' method='post'>" +
                     "<label>Name: </label><br>" +
-                    "<input type='text'/><br>" +
+                    "<input type='text' name='name'/><br>" +
 
                     "<label>My favorite language:</label><br>" +
-                    "<select>" +
+                    "<select name='fChoice'>" +
                         "<option>Java</option>" +
                         "<option>JavaScript</option>" +
                         "<option>Angular</option>" +
                     "</select><br>" +
 
                     "<label>My second favorite language:</label><br>" +
-                    "<select>" +
+                    "<select name='sChoice'>" +
                         "<option>Java</option>" +
                         "<option>JavaScript</option>" +
                         "<option>Angular</option>" +
                     "</select><br>" +
 
                     "<label>My third favorite language:</label><br>" +
-                    "<select>" +
+                    "<select name='tChoice'>" +
                         "<option>Java</option>" +
                         "<option>JavaScript</option>" +
                         "<option>Angular</option>" +
-                    "</select>" +
+                    "</select><br>" +
+                    "<input type='submit' value='submit' />" +
                 "</form>";
         return form;
+    }
+
+//    @RequestMapping(method = {RequestMethod.GET,RequestMethod.POST})
+    @PostMapping(path = "/")
+    public String displayChoice(@RequestParam String name, @RequestParam String fChoice, @RequestParam String sChoice, @RequestParam String tChoice){
+        String displayUserChoice = "" +
+                "<h2>"+name+"</h2>" +
+                "<ol>" +
+                "<li>"+fChoice+"</li>"+
+                "<li>"+sChoice+"</li>"+
+                "<li>"+tChoice+"</li>" +
+                "</ol>";
+        return displayUserChoice;
     }
 }
